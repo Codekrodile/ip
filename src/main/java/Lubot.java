ba36e9e3a82f5a7f76480015b370604b9669753a
Codekrodile <codekrodile@lucy>
2025-01-24 12:58:54 +0800

    added list

--- src/main/java/Lubot.java
+++ src/main/java/Lubot.java
@@ -10,15 +10,17 @@ public class Lubot {
         + "        \\/         \\/             \n";
 
         String horizontalBar = "____________________________________________\n";
+        String[] list = new String[100];
+        int n = 0; // list index
 
         // start
         System.out.println(logo);
         System.out.println("lubot: greetings master, how can i be of service today?");
+        System.out.println("lubot: type 'list' to see list");
         System.out.println("lubot: type 'exit' to exit");
         System.out.println(horizontalBar);
 
-
-        // echo
+        // main
         Scanner scanner = new Scanner(System.in);
         
         while (true) {
@@ -30,11 +32,22 @@ public class Lubot {
                 break;
             }
 
+            // check if users wants to see list
+            if (userInput.equalsIgnoreCase("list")) {
+                System.out.println("lubot:");
+                for (int i=0; i<n; i++) {
+                    System.out.println("  - " + list[i]);
+                }
+                System.out.println(horizontalBar);
+                continue;
+            }
+
             // response
-            System.out.println("lubot: " + userInput);
+            list[n] = userInput;
+            n++;
+            System.out.println("lubot: added '" + userInput + "'");
             System.out.println(horizontalBar);
         }
-
 
         // end
         System.out.println("lubot: pls come again");
