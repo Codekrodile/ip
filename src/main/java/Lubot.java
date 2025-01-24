aac0ca837347975bc5b571b4cdcef7ef0e607ba4
Codekrodile <codekrodile@lucy>
2025-01-24 12:36:26 +0800

    birth of lubot

---
+++ src/main/java/Lubot.java
@@ -0,0 +1,22 @@
+// public class Duke {
+public class Lubot {
+    public static void main(String[] args) {
+        String logo = ".____         ___.           __   \n"
+        + "|    |    __ _\\_ |__   _____/  |_ \n"
+        + "|    |   |  |  \\ __ \\ /  _ \\   __\\\n"
+        + "|    |___|  |  / \\_\\ (  <_> )  |  \n"
+        + "|_______ \\____/|___  /\\____/|__|  \n"
+        + "        \\/         \\/             \n";
+
+        String horizontalBar = "____________________________________________";
+
+        System.out.println(logo);
+        System.out.println("greetings master, how can i be of service today?");
+        System.out.println(horizontalBar);
+        System.out.println("pls come again");
+
+    }
+}
+
+
+
