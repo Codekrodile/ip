d5c1aa2e39b021da3af8a93bbd64303fdf9fc831
Codekrodile <Codekrodile@gmail.com>
2025-02-15 18:42:57 +0800

    finished Storage.java

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,84 +1,100 @@
-import java.io.Files;
-import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Storage {
-    private static final String FILE_PATH = "./data/lubot.txt";
+    private static final Path FILE_PATH = Paths.get("./data/lubot.txt");
 
-    public static void saveTasks(ArrayList<Task> tasks) {
+    public static void ensureFileExists() {
         try {
-            File file = new File(FILE_PATH);
-            file.getParentFile().mkdirs(); // create data folder if it doesnt exist
-            
-            // write
-            FileWriter writer = new FileWriter(file);
-            for (Task task : tasks) {
-                writer.write(taskToString(task) + "\n");
+            if (Files.notExists(FILE_PATH.getParent())) {
+                Files.createDirectories(FILE_PATH.getParent());
             }
-            writer.close();
-
+            if (Files.notExists(FILE_PATH)) {
+                Files.createFile(FILE_PATH);
+            }
         } catch (IOException e) {
             System.out.println("error: " + e.getMessage());
         }
     }
 
-    private String taskToString(Task task) {
+    public static void saveTasks(ArrayList<Task> tasks) {
+        try {
+            List<String> taskStrings = new ArrayList<>();
+
+            for (Task t : tasks) {
+                taskStrings.add(taskToString(t));
+            }
+
+            Files.write(FILE_PATH, taskStrings);
+        } catch (IOException e) {
+            System.out.println("error: " + e.getMessage());
+        }
+    }
+
+    private static String taskToString(Task task) {
         /*
          * [T][ ] borrow book
          * [D][ ] return book  (by:  Sunday)
          * [E][x] collect angpao  (from: wed to:  thu)
          */
         String taskString = task.toString();
+        boolean isDone = taskString.charAt(4) == 'x';
 
         // Todo
         if (task instanceof Todo) {
-            return "T | " + taskString.charAt(4)=='X'? 1:0 + " | " + taskString.substring(7);
+            return String.format("T | %s | %s", 
+                    isDone? "1":"0", 
+                    taskString.substring(7));
         }
 
         // Deadline
         if (task instanceof Deadline) {
-            int byIndex = taskString.indexOf("(by: ");
-            return "D | " + taskString.charAt(4)=='X'? 1:0 + " | " + taskString.substring(7, byIndex) + " | " + taskString.substring(byIndex + 5, taskString.length() - 1);
+            int byIndex = taskString.indexOf(" (by: ");
+
+            return String.format("D | %s | %s | %s", 
+                    isDone? "1":"0",
+                    taskString.substring(7, byIndex),
+                    taskString.substring(byIndex + 6, taskString.length() - 1));
         }
 
         // Event
-        int fromIndex = taskString.indexOf("(from: ");
+        int fromIndex = taskString.indexOf(" (from: ");
         int toIndex = taskString.indexOf(" to: ");
-        return "E | " + taskString.charAt(4)=='X'? 1:0 + " | " + taskString.substring(7, fromIndex) + " | " + taskString.substring(fromIndex + 7, toIndex) + " | " + taskString.substring(toIndex + 5, taskString.length() - 1);
+
+        return String.format("E | %s | %s | %s | %s",
+                isDone? "1":"0",
+                taskString.substring(7, fromIndex),
+                taskString.substring(fromIndex + 8, toIndex),
+                taskString.substring(toIndex + 5, taskString.length() - 1));
     }
 
-    public ArrayList<Task> loadTasks() {
+    public static ArrayList<Task> loadTasks() {
         ArrayList<Task> tasks = new ArrayList<>();
-        File file = new File(FILE_PATH);
-
-        // check if file exists
-        if (!file.exists()) {
-            System.out.println("No previous tasks found. Starting fresh!");
-            return tasks;
-        }
+        ensureFileExists();
 
         try {
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                Task task = stringToTask(line);
+            List<String> taskStrings = Files.readAllLines(FILE_PATH);
 
-                if (task != null) {
-                    tasks.add(task);
+            for (String s : taskStrings) {
+                Task t = stringToTask(s);
+
+                if (t != null) {
+                    tasks.add(t);
                 }
             }
-
-            scanner.close();
-
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             System.out.println("Error reading file: " + e.getMessage());
         }
 
         return tasks;
     }
 
-    public Task stringToTask(String s) {
+    public static Task stringToTask(String s) {
         String[] parts = s.split(" \\| ");
 
         // check for error
@@ -86,22 +102,29 @@ public class Storage {
             return null;
         }
 
+        boolean isDone = parts[1].equals("1");
+
         // Todo
-        if (parts[0] == 'T') {
-            if (parts[1] == 0) {
-                return new Todo(parts[2]).markUndone();
+        if (parts[0].equals("T")) {
+            if (isDone) {
+                return new Todo(parts[2]).markDone();
             }
             return new Todo(parts[2]);
         }
 
         // Deadline
-        if (parts[0] == 'D') {
-            return new Deadline(" ", " ");
+        if (parts[0].equals("D")) {
+            if (isDone) {
+                return new Deadline(parts[2], parts[3]).markDone();
+            }
+            return new Deadline(parts[2], parts[3]);
         }
 
         // Event
-        return new Event(" ", " ", " ");
+        if (isDone) {
+            return new Event(parts[2], parts[3], parts[4]).markDone();
+        }
+        return new Event(parts[2], parts[3], parts[4]);
     }
-
 }
 
